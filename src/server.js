5b7f94dbb1dd22169ca359cb29c6ee75f881b428
Luke Schwarting <jeluker2@gmail.com>
2019-03-02 21:03:27 -0500

    Working comments page done! Able to create multiple issues and add comments to them and respond!

--- src/server.js
+++ src/server.js
@@ -1,21 +1,21 @@
-const http = require('http'); // http module
-const url = require('url'); // url module
-const query = require('querystring'); // query string module
-const htmlHandler = require('./htmlResponses.js');
-const jsonHandler = require('./jsonResponse.js');
+const http = require("http"); // http module
+const url = require("url"); // url module
+const query = require("querystring"); // query string module
+const htmlHandler = require("./htmlResponses.js");
+const jsonHandler = require("./jsonResponse.js");
 
 const port = process.env.PORT || process.env.NODE_PORT || 3000;
 
 const urlStruct = {
-  '/': htmlHandler.getIndex,
+  "/": htmlHandler.getIndex,
   index: htmlHandler.getIndex,
-  '/style.css': htmlHandler.getCSS,
-  '/success': jsonHandler.getSuccess,
-  '/badRequest': jsonHandler.getBadRequest,
-  '/unauthorized': jsonHandler.getUnauthorized,
-  '/forbidden': jsonHandler.getForbidden,
-  '/internal': jsonHandler.getInternal,
-  '/notImplemented': jsonHandler.getNotImplemented,
+  "/style.css": htmlHandler.getCSS,
+  "/success": jsonHandler.getSuccess,
+  "/badRequest": jsonHandler.getBadRequest,
+  "/unauthorized": jsonHandler.getUnauthorized,
+  "/forbidden": jsonHandler.getForbidden,
+  "/internal": jsonHandler.getInternal,
+  "/notImplemented": jsonHandler.getNotImplemented
 };
 
 // function to handle requests
@@ -24,40 +24,43 @@ const onRequest = (request, response) => {
   // returns an object of url parts by name
   const parsedUrl = url.parse(request.url);
 
+  console.log("parsedUrl");
+  console.log(parsedUrl);
+
   // grab the query parameters (?key=value&key2=value2&etc=etc)
   // and parse them into a reusable object by field name
   const params = query.parse(parsedUrl.query);
 
   switch (request.method) {
-    case 'GET':
-      if (parsedUrl.pathname === '/') {
+    case "GET":
+      if (parsedUrl.pathname === "/") {
         // if homepage, send index
         htmlHandler.getIndex(request, response);
-      } else if (parsedUrl.pathname === '/style.css') {
+      } else if (parsedUrl.pathname === "/style.css") {
         // if stylesheet, send stylesheet
         htmlHandler.getCSS(request, response);
-      } else if (parsedUrl.pathname === '/editIssue.html') {
+      } else if (parsedUrl.pathname === "/editIssue.html") {
         // if stylesheet, send stylesheet
         htmlHandler.getEditIssuePage(request, response);
-      } else if (parsedUrl.pathname === '/getIssues') {
+      } else if (parsedUrl.pathname === "/getIssues") {
         // if get users, send user object back
         jsonHandler.getIssues(request, response);
-      } else if (parsedUrl.pathname === '/success') {
+      } else if (parsedUrl.pathname === "/success") {
         // if success is selected
         urlStruct[parsedUrl.pathname](request, response);
-      } else if (parsedUrl.pathname === '/badRequest') {
+      } else if (parsedUrl.pathname === "/badRequest") {
         // if success is selected
         urlStruct[parsedUrl.pathname](request, response, params);
-      } else if (parsedUrl.pathname === '/unauthorized') {
+      } else if (parsedUrl.pathname === "/unauthorized") {
         // if success is selected
         urlStruct[parsedUrl.pathname](request, response, params);
-      } else if (parsedUrl.pathname === '/forbidden') {
+      } else if (parsedUrl.pathname === "/forbidden") {
         // if success is selected
         urlStruct[parsedUrl.pathname](request, response);
-      } else if (parsedUrl.pathname === '/internal') {
+      } else if (parsedUrl.pathname === "/internal") {
         // if success is selected
         urlStruct[parsedUrl.pathname](request, response);
-      } else if (parsedUrl.pathname === '/notImplemented') {
+      } else if (parsedUrl.pathname === "/notImplemented") {
         // if success is selected
         urlStruct[parsedUrl.pathname](request, response);
       } else {
@@ -65,8 +68,8 @@ const onRequest = (request, response) => {
         jsonHandler.getNotFound(request, response);
       }
       break;
-    case 'HEAD':
-      if (parsedUrl.pathname === '/getIssues') {
+    case "HEAD":
+      if (parsedUrl.pathname === "/getIssues") {
         // if get users, send meta data back
         jsonHandler.getIssuesMeta(request, response);
       } else {
@@ -74,9 +77,10 @@ const onRequest = (request, response) => {
         jsonHandler.notFoundMeta(request, response);
       }
       break;
-    case 'POST':
+    case "POST":
+      console.log("in post");
       // if post is to /addUser (our only POST url)
-      if (parsedUrl.pathname === '/addIssue') {
+      if (parsedUrl.pathname === "/addIssue") {
         const res = response;
 
         // uploads come in as a byte stream that we need
@@ -85,7 +89,7 @@ const onRequest = (request, response) => {
 
         // if the upload stream errors out, just throw a
         // a bad request and send it back
-        request.on('error', (err) => {
+        request.on("error", err => {
           console.dir(err);
           res.statusCode = 400;
           res.end();
@@ -93,12 +97,12 @@ const onRequest = (request, response) => {
 
         // on 'data' is for each byte of data that comes in
         // from the upload. We will add it to our byte array.
-        request.on('data', (chunk) => {
+        request.on("data", chunk => {
           body.push(chunk);
         });
 
         // on end of upload stream.
-        request.on('end', () => {
+        request.on("end", () => {
           // combine our byte array (using Buffer.concat)
           // and convert it to a string value (in this instance)
           const bodyString = Buffer.concat(body).toString();
@@ -109,6 +113,42 @@ const onRequest = (request, response) => {
 
           // pass to our addUser function
           jsonHandler.addIssue(request, res, bodyParams);
+        });
+      } else if (parsedUrl.pathname === "/addComment") {
+        // if post is to /addComment (our only POST url)
+        console.log("addComment for post");
+        const res = response;
+
+        // uploads come in as a byte stream that we need
+        // to reassemble once it's all arrived
+        const body = [];
+
+        // if the upload stream errors out, just throw a
+        // a bad request and send it back
+        request.on("error", err => {
+          console.dir(err);
+          res.statusCode = 400;
+          res.end();
+        });
+
+        // on 'data' is for each byte of data that comes in
+        // from the upload. We will add it to our byte array.
+        request.on("data", chunk => {
+          body.push(chunk);
+        });
+
+        // on end of upload stream.
+        request.on("end", () => {
+          // combine our byte array (using Buffer.concat)
+          // and convert it to a string value (in this instance)
+          const bodyString = Buffer.concat(body).toString();
+          // since we are getting x-www-form-urlencoded data
+          // the format will be the same as querystrings
+          // Parse the string into an object by field name
+          const bodyParams = query.parse(bodyString);
+
+          // pass to our addUser function
+          jsonHandler.addComment(request, res, bodyParams);
         });
       }
       break;
